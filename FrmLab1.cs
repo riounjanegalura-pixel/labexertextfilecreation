7b8616bba84949d35d1949f801e1cb360cdb4726
Rio Unjane Galura <riounjanegalura@gmail.com>
2025-10-17 19:51:25 +0800

    create button code

--- FrmLab1.cs
+++ FrmLab1.cs
@@ -20,7 +20,18 @@ namespace labexertextfilecreation
 
         private void btnCreate_Click(object sender, EventArgs e)
         {
-         
+            FrmFileName frmFileName = new FrmFileName();
+            frmFileName.ShowDialog();
+
+            string getInput = txtInput.Text;
+
+            string docPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+
+            using (StreamWriter outputFile = new StreamWriter(Path.Combine(docPath, FrmFileName.SetFileName)))
+            {
+                outputFile.WriteLine(getInput);
+                Console.WriteLine(getInput);
+            }
         }
     }
 }
